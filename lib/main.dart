beb69249ea7dc20be6c3504e323951cc2b87cb90
THEPHATIH\fatih <fatih.atese@yahoo.com>
2023-08-09 12:14:23 +0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,8 +28,49 @@ class BenimUygulamam extends StatelessWidget {
   }
 }
 
-class YemekSayfasi extends StatelessWidget {
-  const YemekSayfasi({super.key});
+class YemekSayfasi extends StatefulWidget {
+  YemekSayfasi({super.key});
+
+  @override
+  State<YemekSayfasi> createState() => _YemekSayfasiState();
+}
+
+class _YemekSayfasiState extends State<YemekSayfasi> {
+  int corbaNo = 1;
+
+  int yemekNo = 1;
+
+  int tatliNo = 1;
+
+  List<String> corbaAdlari = [
+    'Mercimek',
+    'Tarhana',
+    'Tavuk Suyu',
+    'Düğün Çorbası',
+    'Yoğurtlu Çorba'
+  ];
+  List<String> yemekAdlari = [
+    'Karnıyarık',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli Köfte',
+    'Izgara Balık'
+  ];
+  List<String> tatliAdlari = [
+    'Kadayıf',
+    'Baklava',
+    'Sütlaç',
+    'Kazandibi',
+    'Dondurma'
+  ];
+
+  void karistir() {
+    setState(() {
+      corbaNo = Random().nextInt(5) + 1;
+      yemekNo = Random().nextInt(5) + 1;
+      tatliNo = Random().nextInt(5) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,22 +81,88 @@ class YemekSayfasi extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(12.0),
               child: TextButton(
-                  onPressed: () {
-                    print('corba buton calisiyor');
-                  },
-                  child: Image.asset('assets/corba_1.jpg')),
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateProperty.all(Colors.yellow),
+                  ),
+                  onPressed: karistir,
+                  child: Image.asset('assets/corba_$corbaNo.jpg')),
+            ),
+          ),
+          Text(
+            corbaAdlari[corbaNo - 1],
+            style: TextStyle(fontSize: 15),
+          ),
+          Container(
+            width: 200,
+            child: const Divider(
+              height: 10,
+              color: Colors.black,
+              thickness: 2.0,
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/yemek_1.jpg'),
+              child: TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.yellow),
+                ),
+                onPressed: karistir,
+                child: Image.asset('assets/yemek_$yemekNo.jpg'),
+              ),
+            ),
+          ),
+          Text(
+            yemekAdlari[yemekNo - 1],
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          Container(
+            width: 200,
+            child: const Divider(
+              height: 10,
+              color: Colors.black,
+              thickness: 2.0,
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/tatli_1.jpg'),
+              child: TextButton(
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateProperty.all(Colors.yellow),
+                  ),
+                  onPressed: karistir,
+                  child: Image.asset('assets/tatli_$tatliNo.jpg')),
+            ),
+          ),
+          Text(
+            tatliAdlari[tatliNo - 1],
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          Container(
+            width: 200,
+            child: const Divider(
+              height: 10,
+              color: Colors.black,
+              thickness: 2.0,
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: MaterialStateProperty.all(Colors.yellow),
+              ),
+              onPressed: karistir,
+              child: Text(
+                'KARIŞTIR',
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
             ),
           ),
         ],
